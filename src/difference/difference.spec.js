5447bf6892af20e6fec170e74f536b1a5bcf87c5
mervat abuamro <mervat.abuamro@gmail.com>
2023-07-29 10:22:10 +0200

    testing the difference function

---
+++ src/difference/difference.spec.js
@@ -0,0 +1,21 @@
+import { differenceArray } from './difference.js';
+
+describe('compare two arrays and write different values in new one', () => {
+  it('compare empty arrays', () => {
+    const array = []
+    const values = []
+    expect(differenceArray(array, values)).toEqual([]);
+  });
+  it('arrays of numbers', () => {
+    const array = [1, 5, 3];
+    const values = [1, 4, 3];
+    expect(differenceArray(array, values)).toEqual([5]);
+  });
+
+  it('arrays of strings', () => {
+    const array = ['apple', 'banana', 'orange', 'grape'];
+    const values = ['banana', 'grapefruit', 'pear'];
+    expect(differenceArray(array, values)).toEqual(['apple', 'orange', 'grape']);
+  });
+
+});
